833b9906c0c5133d62fb3a173d74d7cb17dd3c87
Omar Benavides <omar.benavides.rmz@gmail.com>
2023-12-18 23:34:48 -0600

    [fix] Lint

--- src/components/search/SearchResults/SearchResults.tsx
+++ src/components/search/SearchResults/SearchResults.tsx
@@ -19,7 +19,7 @@ export const SearchResults = ({ results, renderCustomContent }: SearchResultsPro
     const itemText = item?.name ?? item?.id ?? JSON.stringify(item);
 
     return (
-      <li key={`search_result_${itemId}`} className="SearchItem">
+      <li key={`search_result_${itemId}`} className='SearchItem'>
         {renderCustomContent?.(item) ?? <p>{itemText}</p>}
       </li>
     );
